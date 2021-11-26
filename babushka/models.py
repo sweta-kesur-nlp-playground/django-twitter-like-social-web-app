f8c114998d8221ebd3052aa8460ce99b98533951
Sweta Kesur <skesur@purdue.edu>
2021-11-26 16:56:38 -0500

    custom registration added fields to models.py

--- babushka/models.py
+++ babushka/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	username = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	gender = models.CharField(max_length=100)
 	is_author = models.BooleanField(default=True)
 	is_reader = models.BooleanField(default=False)
 	gender = models.CharField(max_length=100)
