be4804ae186c20c32fa8a9a883e20132b9de2edd
Sweta Kesur <skesur@purdue.edu>
2021-11-26 15:44:34 -0500

    added CustomUserForm

--- babushka/admin.py
+++ babushka/admin.py
@@ -10,6 +10,19 @@ class CustomUserAdmin(UserAdmin):
 	model = CustomUser
 	add_form = CustomUserCreationForm
 
+	fieldsets = (
+		*UserAdmin.fieldsets,
+		(
+			'User role',
+			{
+				'fields': (
+					'is_author',
+					'is_reader',
+				)
+			}
+		)
+	)
+
 class CommentAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email')
 	list_filter = ('name',)
@@ -21,6 +34,6 @@ class MessageAdmin(admin.ModelAdmin):
 admin.site.register(Comment, CommentAdmin)
 admin.site.register(Message, MessageAdmin)
 admin.site.register(BookUser)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.unregister(Group)
 
--- babushka/forms.py
+++ babushka/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from .models import Comment, Message, BookUser, CustomUser
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment, Message, BookUser, CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
 	class Meta:
 		model = CustomUser
-		fields = "__all__"
+		# fields = "__all__"
+		fields = ["username", "first_name", "last_name", "email", "password", "gender", "is_author", "is_reader"]
 
 class CommentForm(forms.ModelForm):
 
