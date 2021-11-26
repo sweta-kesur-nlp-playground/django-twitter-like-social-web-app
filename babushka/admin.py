a519a9d19f305bd92fe268943f38c5aae3b9852b
Sweta Kesur <skesur@purdue.edu>
2021-11-26 14:51:17 -0500

    edited user inbuilt model

--- babushka/admin.py
+++ babushka/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import Comment, Message, BookUser, CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
 admin.site.site_header = 'Babushka Dashboard'
+
+class CustomUserAdmin(UserAdmin):
+	model = CustomUser
+	add_form = CustomUserCreationForm
 
 class CommentAdmin(admin.ModelAdmin):
 	list_display = ('name', 'email')
--- babushka/forms.py
+++ babushka/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from .models import Comment, Message, BookUser
+from .models import Comment, Message, BookUser, CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+
+	class Meta:
+		model = CustomUser
+		fields = "__all__"
 
 class CommentForm(forms.ModelForm):
 
