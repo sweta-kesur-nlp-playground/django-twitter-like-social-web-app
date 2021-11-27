ff67c1dc89ca03cddd11acd1e2f8caf42e87ea7e
Sweta Kesur <skesur@purdue.edu>
2021-11-27 15:45:08 -0500

    auto profile creation on registration

---
+++ babushka/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'babushka.apps.BabushkaConfig'
--- babushka/apps.py
+++ babushka/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BabushkaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'babushka'
+
+    def ready(self):
+        import babushka.signals
